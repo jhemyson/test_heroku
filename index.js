75b8eac0da5c66820b71bc6901520f296d5affbe
jhemyson <jhemyson_@hotmail.com>
2019-06-26 09:52:59 -0300

    app start

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true
 });
 
-app.use("", require("./app/routes"));
+app.use("", require("./src/app/routes"));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
